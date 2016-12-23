11c889fccc07c8605f758c962834fac328d6c023
cwh0594 <cwh0594@175game.com>
2016-12-23 13:45:19 +0800

    add tools.

--- init.el
+++ init.el
@@ -144,7 +144,7 @@ values."
    ;; with `:variables' keyword (similar to layers). Check the editing styles
    ;; section of the documentation for details on available variables.
    ;; (default 'vim)
-   dotspacemacs-editing-style 'vim
+   dotspacemacs-editing-style 'emacs
    ;; If non nil output loading progress in `*Messages*' buffer. (default nil)
    dotspacemacs-verbose-loading nil
    ;; Specify the startup banner. Default value is `official', it displays
@@ -353,6 +353,58 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
+  ;; Personal Information Configuration
+  (setq user-full-name "wowchan")
+  (setq user-email-address "wow4chan@gmail.com")
+
+  ;; 自动在文件最后加一个空行
+  (setq require-final-newline t)
+
+  ;; 设置不自动保存
+  (setq auto-save-mode nil)
+  ;; 设置不生成#filename#临时文件
+  (setq auto-save-default nil)
+  ;; 设置不生成备份文件
+  (setq make-backup-files nil)
+
+  ;; 多行移动
+  (global-set-key (kbd "C-M-n")
+                  (lambda () (interactive) (next-line 5)))
+  (global-set-key (kbd "C-M-p")
+                  (lambda () (interactive) (previous-line 5)))
+
+  ;; 设置单行的复制以剪切
+  (defadvice kill-ring-save (before slickcopy activate compile)
+    (interactive
+     (if mark-active (list (region-beginning) (region-end))
+       (list (line-beginning-position)
+             (line-beginning-position 2)))))
+  (defadvice kill-region (before slickcut activate compile)
+    (interactive
+     (if mark-active (list (region-beginning) (region-end))
+       (list (line-beginning-position)
+             (line-beginning-position 2)))))
+
+  ;; 快捷注释
+  (defun optimized-comment-dwim-line (&optional arg)
+    (interactive "*P")
+    (comment-normalize-vars)
+    (if (and (not (region-active-p)) (not (looking-at "[ \t]*$")))
+        (comment-or-uncomment-region (line-beginning-position) (line-end-position))
+      (comment-dwim arg)))
+  (global-set-key "\M-;" 'optimized-comment-dwim-line)
+
+  ;; 刷新文件
+  (defun refresh-file ()
+    (interactive)
+    (revert-buffer t (not (buffer-modified-p)) t))
+
+  (global-set-key (kbd "M-m m o r") 'refresh-file)
+  (global-set-key (kbd "<f5>") 'refresh-file)
+
+  ;; add keybinding for evil
+  (global-set-key (kbd "<C-f1>") 'turn-on-evil-mode)
+  (global-set-key (kbd "<C-f2>") 'turn-off-evil-mode)
 
   ;; Bind clang-format-region to C-M-tab in all modes:
   (global-set-key [C-M-tab] 'clang-format-region)
@@ -360,12 +412,6 @@ you should place your code here."
   (add-hook 'c++-mode-hook 'clang-format-bindings)
   (defun clang-format-bindings ()
     (define-key c++-mode-map [tab] 'clang-format-buffer))
-
-  ;; add keybinding for evil
-  (global-set-key (kbd "<C-f1>") 'turn-on-evil-mode)
-  (global-set-key (kbd "<C-f2>") 'turn-off-evil-mode)
-
-  (turn-off-evil-mode)
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
