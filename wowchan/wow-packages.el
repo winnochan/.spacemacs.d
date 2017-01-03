8f6a6977863c25a3a5033a295e58be5fa6372a35
cwh0594 <cwh0594@175game.com>
2017-01-03 16:26:50 +0800

    add evil packages

--- wowchan/wow-packages.el
+++ wowchan/wow-packages.el
@@ -14,35 +14,35 @@
 
 (defvar wow-excluded-packages
   '(
-    spacemacs-language
-    spacemacs-theme
-    spacemacs-evil
-    ;; spaceline
+    ;; spacemacs-language
+    ;; spacemacs-theme
+    ;; spacemacs-evil
+    ;; ;; spaceline
 
-    ;; packages for evil
-    evil-anzu
-    evil-args
-    evil-ediff
-    evil-exchange
-    evil-iedit-state
-    evil-indent-plus
-    evil-lisp-state
-    evil-mc
-    evil-nerd-commenter
-    evil-matchit
-    evil-numbers
-    evil-search-highlight-persist
-    evil-surround
-    evil-tutor
-    evil-unimpaired
-    evil-visual-mark-mode
-    evil-visualstar
-    hs-minor-mode
-    linum-relative
-    vi-tilde-fringe
+    ;; ;; packages for evil
+    ;; evil-anzu
+    ;; evil-args
+    ;; evil-ediff
+    ;; evil-exchange
+    ;; evil-iedit-state
+    ;; evil-indent-plus
+    ;; evil-lisp-state
+    ;; evil-mc
+    ;; evil-nerd-commenter
+    ;; evil-matchit
+    ;; evil-numbers
+    ;; evil-search-highlight-persist
+    ;; evil-surround
+    ;; evil-tutor
+    ;; evil-unimpaired
+    ;; evil-visual-mark-mode
+    ;; evil-visualstar
+    ;; hs-minor-mode
+    ;; linum-relative
+    ;; vi-tilde-fringe
 
     ;; packages for magit
-    evil-magit
+    ;; evil-magit
     ;; evil-escape
     )
   )
