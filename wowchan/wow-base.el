c9bd2aa26fa8e227e500f8519234de7fa0b3c460
wowchan <ckingwowchan@gmail.com>
2017-01-13 20:54:23 +0800

    add org publish blog config

--- wowchan/wow-base.el
+++ wowchan/wow-base.el
@@ -90,4 +90,25 @@
 (global-whitespace-mode 1)
 ;; (whitespace-mode)
 
+(setq org-publish-project-alist
+      '(
+        ;; config
+        ("blog-notes"
+         :base-directory "~/org/notes"
+         :base-extension "org"
+         :publishing-directory "~/org/public_html/"
+         :recursive t
+         :publishing-function org-html-publish-to-html
+         :author "WowChan"
+         :email "wow4chan@gmail.com")
+        ("blog-static"
+         :base-directory "~/org/notes"
+         :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+         :publishing-directory "~/org/public_html/"
+         :recursive t
+         :publishing-function org-publish-attachment)
+        ("blog"
+         :components ("blog-notes" "blog-static"))
+        ))
+
 (provide 'wow-base)
