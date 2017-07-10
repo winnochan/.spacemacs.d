dc7546443c37984fef23d496f6eb1d040d05e2d8
陈万豪 <wow4chan@gmail.com>
2017-07-10 09:57:26 +0800

    optimize user config

--- spacemacs-user-config.el
+++ spacemacs-user-config.el
@@ -2,6 +2,12 @@
 (setq user-full-name "wowchan")
 (setq user-email-address "wow4chan@gmail.com")
 
+;; 多行移动
+(global-set-key (kbd "C-M-n")
+                (lambda () (interactive) (next-line 7)))
+(global-set-key (kbd "C-M-p")
+                (lambda () (interactive) (previous-line 7)))
+
 ;; refresh-file
 (defun refresh-file ()
   (interactive)
@@ -14,5 +20,17 @@
 (setq default-tab-width 4)
 
 (global-spacemacs-whitespace-cleanup-mode t)
+
+;; 设置单行的复制以剪切
+(defadvice kill-ring-save (before slickcopy activate compile)
+  (interactive
+   (if mark-active (list (region-beginning) (region-end))
+     (list (line-beginning-position)
+           (line-beginning-position 2)))))
+(defadvice kill-region (before slickcut activate compile)
+  (interactive
+   (if mark-active (list (region-beginning) (region-end))
+     (list (line-beginning-position)
+           (line-beginning-position 2)))))
 
 (provide 'spacemacs-user-config)
