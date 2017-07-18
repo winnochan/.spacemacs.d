d7e716addf28293d3336f3397f7065513d959668
wowchan <wow4chan@gmail.com>
2017-07-19 01:34:25 +0800

    optimize rust format config

--- spacemacs-user-config.el
+++ spacemacs-user-config.el
@@ -37,6 +37,6 @@
      (list (line-beginning-position)
            (line-beginning-position 2)))))
 
-(add-hook 'rust-mode-hook (lambda () (rust-enable-format-on-save)))
+(setq rust-format-on-save t)
 
 (provide 'spacemacs-user-config)
