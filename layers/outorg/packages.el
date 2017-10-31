a3d588f6a24bae4df81c30a44566899c0e7b88b0
陈万豪 <wow4chan@gmail.com>
2017-10-31 14:12:08 +0800

    add outorg config

---
+++ layers/outorg/packages.el
@@ -0,0 +1,17 @@
+;;; packages.el --- evil-commentary Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(setq outorg-packages
+      '(outorg))
+
+(defun outorg/init-outorg ()
+  (use-package outorg
+    :init (defvar outline-minor-mode-prefix "\M-#")))
