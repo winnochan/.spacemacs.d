c91cdcd745ad5a108266646cc4275d883787a457
wowchan <wow4chan@gmail.com>
2017-11-15 13:48:48 +0800

    update config

--- user-init.el
+++ user-init.el
@@ -1,7 +1,9 @@
 ;; emacs-china elpa mirrors
 (setq configuration-layer--elpa-archives
-      '(("melpa-cn" . "http://elpa.emacs-china.org/melpa/")
-        ("org-cn"   . "http://elpa.emacs-china.org/org/")
-        ("gnu-cn"   . "http://elpa.emacs-china.org/gnu/")))
+      '(("melpa-stable-cn" . "http://elpa.emacs-china.org/melpa-stable/")
+        ("marmalade-cn"    . "http://elpa.emacs-china.org/marmalade/")
+        ("melpa-cn"        . "http://elpa.emacs-china.org/melpa/")
+        ("org-cn"          . "http://elpa.emacs-china.org/org/")
+        ("gnu-cn"          . "http://elpa.emacs-china.org/gnu/")))
 
 (provide 'spacemacs-user-init)
