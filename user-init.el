2e7ff118a35072f8dc9db372bedb347a970f0c81
WinnoChan <winnochan@icloud.com>
2017-11-27 18:16:03 +0800

    update config

--- user-init.el
+++ user-init.el
@@ -1,9 +1,11 @@
 ;; emacs-china elpa mirrors
-(setq configuration-layer--elpa-archives
-      '(("melpa-stable-cn" . "http://elpa.emacs-china.org/melpa-stable/")
-        ("marmalade-cn"    . "http://elpa.emacs-china.org/marmalade/")
+(setq configuration-layer-elpa-archives
+      '(
+        ("gnu-cn"          . "http://elpa.emacs-china.org/gnu/")
+        ("melpa-stable-cn" . "http://elpa.emacs-china.org/melpa-stable/")
         ("melpa-cn"        . "http://elpa.emacs-china.org/melpa/")
+        ("marmalade-cn"    . "http://elpa.emacs-china.org/marmalade/")
         ("org-cn"          . "http://elpa.emacs-china.org/org/")
-        ("gnu-cn"          . "http://elpa.emacs-china.org/gnu/")))
+        ))
 
 (provide 'spacemacs-user-init)
