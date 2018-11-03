1b711b34142b5f3456e42da03247c62f0e21b1ee
WinnoChan <winnochan@icloud.com>
2018-11-03 10:53:24 +0000

    update config

--- user-init.el
+++ user-init.el
@@ -1,10 +1,13 @@
 ;; emacs-china elpa mirrors
 (setq configuration-layer-elpa-archives
-      '(("gnu-cn"          . "http://elpa.emacs-china.org/gnu/")
+      '(
+        ("gnu-cn"          . "http://elpa.emacs-china.org/gnu/")
         ;; ("melpa-stable-cn" . "http://elpa.emacs-china.org/melpa-stable/")
         ("melpa-cn"        . "http://elpa.emacs-china.org/melpa/")
         ;; ("marmalade-cn"    . "http://elpa.emacs-china.org/marmalade/")
-        ("org-cn"          . "http://elpa.emacs-china.org/org/")))
+        ("org-cn"          . "http://elpa.emacs-china.org/org/")
+        )
+      )
 
 ;; tramp-ssh
 ;; (setq tramp-mode nil)
