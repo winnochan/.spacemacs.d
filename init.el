79d13f87757b84329f240409059f0352135f9e1a
陈万豪 <cwh0594@175game.com>
2018-11-17 10:36:47 +0800

    update config

--- init.el
+++ init.el
@@ -382,11 +382,11 @@ It should only modify the values of Spacemacs settings."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(spacegray
+   dotspacemacs-themes '(ample-zen
                          ample-flat
+                         zen-and-art
                          hc-zenburn
                          zenburn
-                         zen-and-art
                          spacemacs-light
                          spacemacs-dark)
 
