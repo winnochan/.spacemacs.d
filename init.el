b37394ef31ec880a0f56e9a701dbd0cb6b91becb
陈万豪 <cwh0594@175game.com>
2018-11-17 10:29:27 +0800

    update config

--- init.el
+++ init.el
@@ -235,6 +235,9 @@ This function should only modify configuration layer settings."
             ;; helm-gtags-cygwin-use-global-w32-port nil
             )
 
+     ;; themes
+     (themes-megapack)
+
      ;; tools
      command-log
      fasd
@@ -379,7 +382,12 @@ It should only modify the values of Spacemacs settings."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(spacemacs-light
+   dotspacemacs-themes '(spacegray
+                         ample-flat
+                         hc-zenburn
+                         zenburn
+                         zen-and-art
+                         spacemacs-light
                          spacemacs-dark)
 
    ;; Set the theme for the Spaceline. Supported themes are `spacemacs',
